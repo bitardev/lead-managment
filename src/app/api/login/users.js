1079999a43ce3477ad83c03e179abc12d7e871ba
Bitar Youssef <bitardev@gmail.com>
2024-04-22 15:43:38 +0100

    First commit

---
+++ src/app/api/login/users.js
@@ -0,0 +1,20 @@
+// ** Fake user data and data type
+// =============== Fake Data ============================
+export const users = [
+  {
+    id: 1,
+    name: 'John Doe',
+    password: 'admin',
+    email: 'admin@vuexy.com',
+    image: '/images/avatars/1.png',
+    role: 'admin'
+  },
+  {
+    id: 2,
+    name: 'Michael Maurin',
+    password: 'agent',
+    email: 'agent@vuexy.com',
+    image: '/images/avatars/2.png',
+    role: 'agent'
+  }
+]
